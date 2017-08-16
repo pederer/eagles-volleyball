8bd5a5b44defc6e9fb02ec3a9d29235838d3fd40
pederer <patrick.ederer@gmail.com>
2017-08-15 23:01:22 -0400

    15AUG17 update to the schedule

--- js/volleyball.js
+++ js/volleyball.js
@@ -6,7 +6,7 @@ $(function() {
 	// Date, Time, Location, Opponent
 	var schedule = [
 		['18 Aug', '4:30p', '(JV Only) 1365 Woodville Pike, Milford, OH 45150', 'Milford Christian Bulldogs'],
-		['22 Aug', '5:30p', 'Southern State, 100 Hobart Drive Hillsboro, OH 45133', 'Hillsboro Christian Crusaders'],
+		['22 Aug', '5:30p', 'Hillsboro Christian Academy, 849 South High St. Hillsboro, OH 45133', 'Hillsboro Christian Crusaders'],
 		['26 Aug', '9:00a', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Trailblazers Tournament'],
 		['29 Aug', '6:30p', 'Wilmington High School', 'Fayette Christian Crusaders'],
 		['31 Aug', '6:00p', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Cincinnati Trailblazers'],
@@ -16,9 +16,10 @@ $(function() {
 		['21 Sept', '6:00p', 'Wilmington High School', 'Cincinnati Trailblazers'],
 		['26 Sept', '5:00p', '1315 Dayton Ave NW, Washington Ct Hs, OH 43160', 'Fayette Christian Crusaders'],
 		['2 Oct', '6:00p', '4900 Old Irwin Simpson Rd, Mason, OH 45040', 'Liberty Bible Academy Patriots'],
-		['3 Oct', '6:30p', 'Wilmington High School', 'Hillsboro Christian Crusaders'],
+:		['3 Oct', '7:15p', 'Wilmington High School', 'Hillsboro Christian Crusaders'],
 		['10 Oct', '5:00p', '3655 E. Patterson Rd, Beavercreek, OH 45430', 'Dayton Golden Angels'],
-		['13 Oct', '6:30p', 'Wilmington High School', 'Miami Valley Saints']
+		['13 Oct', '6:30p', 'Wilmington High School', 'Miami Valley Saints'],
+		['21 Oct', '?:??', 'Hillsboro Christian Academy, 849 South High St. Hillsboro, OH 45133', 'Hillsboro Tournament']
 	];
 	// var schedule = [
 	// 	['27 Aug', '9:00a', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Trailblazers Tournament'],
