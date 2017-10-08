bc16bb4d2bf0f1fb7251c440e4171ad24324c32a
pederer <patrick.ederer@gmail.com>
2017-10-08 15:09:46 -0400

    Fixed Dayton Golden Angels game time

--- js/volleyball.js
+++ js/volleyball.js
@@ -17,7 +17,7 @@ $(function() {
 		['26 Sept', '5:00p', '1315 Dayton Ave NW, Washington Ct Hs, OH 43160', 'Fayette Christian Crusaders'],
 		['2 Oct', '5:30p', '(JV Only) Emmanuel Baptist Church, 495 Old State Route 122 Lebanon, OH 45036', 'Liberty Bible Academy & Calvary Academy'],
 		['3 Oct', '7:15p', 'Wilmington High School', 'Hillsboro Christian Crusaders'],
-		['10 Oct', '5:00p', '3655 E. Patterson Rd, Beavercreek, OH 45430', 'Dayton Golden Angels'],
+		['10 Oct', '5:15p', '3655 E. Patterson Rd, Beavercreek, OH 45430', 'Dayton Golden Angels'],
 		['13 Oct', '6:30p', 'Wilmington High School', 'Miami Valley Saints'],
 		['21 Oct', '9:00a', 'Hillsboro Christian Academy, 849 South High St. Hillsboro, OH 45133', 'Hillsboro Tournament']
 	];
