cd54c7224c6f1c9deb725c6eb9010d4040de6412
pederer <patrick.ederer@gmail.com>
2018-07-23 18:34:11 -0400

    Updated the 2018 game schedule

--- js/volleyball.js
+++ js/volleyball.js
@@ -8,18 +8,22 @@ $(function() {
 		['18 Aug', '??:??', '(Varsity Only) Hillsboro Christian Academy, 849 South High St. Hillsboro, OH 45133', 'Hillsboro Tournament'],
 		['21 Aug', '5:30p', '(JH Only) Milford Christian Academy, 1365 Woodville Pike, Milford, OH 45150', 'Milford Christian Bulldogs'],
 		['25 Aug', '9:00a', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Trailblazers Tournament'],
-	 	['28 Aug', '6:30p', '(Varsity Only) Wilmington High School', 'Emmanuel Baptist Crusaders'],
+	 	['28 Aug', '6:30p', '(Varsity Only) Wilmington Christian Academy (Not WHS)', 'Emmanuel Baptist Crusaders'],
+		['30 Aug', '6:30p', '(Varsity Only) Wilmington High School', 'Milford Christian Bulldogs'],
 		['6 Sept', '6:30p', 'Wilmington High School', 'Fayette Christian Crusaders'],
 		['7 Sept', '??:??', '(Varsity Only) Milford Christian Academy, 1365 Woodville Pike, Milford, OH 45150', 'Milford Tournament'],
 		['7 Sept', '??:??', '(Varsity Only) Milford Christian Academy, 1365 Woodville Pike, Milford, OH 45150', 'Milford Tournament'],
 		['11 Sept', '5:30p', 'Hillsboro Christian Academy, 849 South High St. Hillsboro, OH 45133', 'Hillsboro Christian Crusaders'],
-		['14 Sept', '5:00p', '(JH Only) Milford Christian Academy, 1365 Woodville Pike, Milford, OH 45150', 'Milford Christian Bulldogs'],
-	 	['14 Sept', '6:00p', '(Varsity Only) 495 Old 122 Rd, Lebanon, OH 45036', 'Emmanuel Baptist Crusaders'],
+	 	['14 Sept', '6:00p', '(Varsity Only) Emmanuel Baptist Church, 495 Old 122 Rd, Lebanon, OH 45036', 'Emmanuel Baptist Crusaders'],
+		['14 Sept', '6:30p', '(JH Only) Wilmington High School', 'Milford Christian Bulldogs'],
 		['18 Sept', '5:30p', '1315 Dayton Ave NW, Washington Ct Hs, OH 43160', 'Fayette Christian Crusaders'],
-		['20 Sept', '??:??', '????', 'Miami Valley Saints'],
+		['20 Sept', '5:30p', 'Miami County YMCA (Robinson Branch) 3060 S Co Rd 25A, Troy, OH 45373', 'Miami Valley Saints'],
 		['25 Sept', '6:30p', 'Wilmington High School', 'Cincinnati Trailblazers'],
+		['27 Sept', '5:30p', '(Varsity Only) Horizon Science Academy, 250 Shoup Mill Rd, Dayton, OH 45415', 'Horizon Science Academy '],
 		['2 Oct', '6:30p', 'Wilmington High School', 'Hillsboro Christian Crusaders'],
-		['16 Oct', '5:30p', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Cincinnati Trailblazers']
+		['9 Oct', '6:30p', 'Wilmington High School', 'Miami Valley Saints'],
+		['16 Oct', '5:30p', 'Sports Plus, 10765 Reading Road in Sharonville, 45241', 'Cincinnati Trailblazers'],
+		['18 Oct', '6:30p', '(Varsity Only) Horizon Science Academy, 250 Shoup Mill Rd, Dayton, OH 45415', 'Horizon Science Academy ']
 	];
 	//var schedule = [
 	//	['18 Aug', '4:30p', '(JV Only) Milford Christian Academy, 1365 Woodville Pike, Milford, OH 45150', 'Milford Christian Bulldogs'],
